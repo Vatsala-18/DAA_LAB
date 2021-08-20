b051bda434806b5a4a26ba50a1a89a7a3f0f2365
Vatsala Vats <vatsalavats18@gmail.com>
2021-08-21 01:26:01 +0530

    sequence detection done

---
+++ Week-2/Problem_2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int sequence(int arr[], int n){
+    int i,j,k,f=0;
+    for(i=0;i<n-2;i++){
+        for(j=i+1;j<n-1;j++){
+            for(k=j+1;k<n;k++){
+                if(arr[i]+arr[j] == arr[k]){
+                    cout<<i+1<<","<<j+1<<","<<k+1<<endl;
+                    f=1;
+                    break;
+                }
+            }
+        }
+    }
+    if(f==0)
+        cout<<"No sequence found."<<endl;
+}
+int main(){
+    int t,n,num;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        sequence(arr,n);
+    }
+    return 0;
+}
